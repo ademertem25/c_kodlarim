2616b2546584e1ae1359fd351574cbce590e2d0a
ademertem25 <ademertem58@gmail.com>
2022-08-15 11:30:50 +0300

    short circuit behavior

---
+++ short_circuit_behavior.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int foo(void)
+{
+    printf("foo\n");
+    return 0;
+}
+int bar(void)
+{
+    printf("bar\n");
+    return 2;
+}
+
+int main(void)
+{
+    int x = 0;
+    int y = 7;
+    int z;
+
+    foo()&& bar();
+    z = x && y++;
+
+    printf("x = %d\n",x);
+    printf("y = %d\n",y);
+}
